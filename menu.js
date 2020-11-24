4c8ba9b554323157b007db179c4961193a22521f
jeremyhu134 <52206356+jeremyhu134@users.noreply.github.com>
2020-11-24 14:06:13 -0500

    Add files via upload

--- menu.js
+++ menu.js
@@ -18,102 +18,21 @@ class MainScreen extends Phaser.Scene {
 	}
     preload(){
         this.load.image('joepic','images/joe.png');
+        this.load.image('cake','https://lh3.googleusercontent.com/proxy/1-gSqoTqM5dCd1XD54UpXtmBm3Zh3aSiVKupg2ATQUEkMlYL0aHyzuLj0QvxGq01E5skzkQC4FrIRv-5IRl1HA7z');
         this.load.audio('iamfat','images/iamfat.mp3');
     }
     create() {
-        gameState.code = "";
         codetext = nine = this.add.text( 100, 900, `Code : ${gameState.code}`, {fill: '#000000', fontSize: '100px'});
-        gameState.gamecreated = false;
-        one = this.add.text( 100, 100, `1`, {fill: '#000000', fontSize: '100px'}).setInteractive();
-        two = this.add.text( 400, 100, `2`, {fill: '#000000', fontSize: '100px'}).setInteractive();
-        three = this.add.text( 700, 100, `3`, {fill: '#000000', fontSize: '100px'}).setInteractive();
-        four = this.add.text( 100, 400, `4`, {fill: '#000000', fontSize: '100px'}).setInteractive();
-        five = this.add.text( 400, 400, `5`, {fill: '#000000', fontSize: '100px'}).setInteractive();
-        six = this.add.text( 700, 400, `6`, {fill: '#000000', fontSize: '100px'}).setInteractive();
-        seven = this.add.text( 100, 700, `7`, {fill: '#000000', fontSize: '100px'}).setInteractive();
-        eight = this.add.text( 400, 700, `8`, {fill: '#000000', fontSize: '100px'}).setInteractive();
-        nine = this.add.text( 700, 700, `9`, {fill: '#000000', fontSize: '100px'}).setInteractive();
-        one.on('pointerdown', () => {
-            gameState.code = gameState.code + "1";
-            codetext.destroy();
-            codetext = this.add.text( 100, 900, `Code : ${gameState.code}`, {fill: '#000000', fontSize: '100px'});
-            if(gameState.code.length > 6){
-                gameState.code = "";
-            }
-        });
-        two.on('pointerdown', () => {
-            gameState.code = gameState.code + "2";
-            codetext.destroy();
-            codetext = this.add.text( 100, 900, `Code : ${gameState.code}`, {fill: '#000000', fontSize: '100px'});
-            if(gameState.code.length > 6){
-                gameState.code = "";
-            }
-        });
-        three.on('pointerdown', () => {
-            gameState.code = gameState.code + "3";
-            codetext.destroy();
-            codetext = this.add.text( 100, 900, `Code : ${gameState.code}`, {fill: '#000000', fontSize: '100px'});
-            if(gameState.code.length > 6){
-                gameState.code = "";
-            }
-        });
-        four.on('pointerdown', () => {
-            gameState.code = gameState.code + "4";
-            codetext.destroy();
-            codetext = this.add.text( 100, 900, `Code : ${gameState.code}`, {fill: '#000000', fontSize: '100px'});
-            if(gameState.code.length > 6){
-                gameState.code = "";
-            }
-        });
-        five.on('pointerdown', () => {
-            gameState.code = gameState.code + "5";
-            codetext.destroy();
-            codetext = this.add.text( 100, 900, `Code : ${gameState.code}`, {fill: '#000000', fontSize: '100px'});
-            if(gameState.code.length > 6){
-                gameState.code = "";
-            }
-        });
-        six.on('pointerdown', () => {
-            gameState.code = gameState.code + "6";
-            codetext.destroy();
-            codetext = this.add.text( 100, 900, `Code : ${gameState.code}`, {fill: '#000000', fontSize: '100px'});
-            if(gameState.code.length > 6){
-                gameState.code = "";
-            }
-        });
-        seven.on('pointerdown', () => {
-            gameState.code = gameState.code + "7";
-            codetext.destroy();
-            codetext = this.add.text( 100, 900, `Code : ${gameState.code}`, {fill: '#000000', fontSize: '100px'});
-            if(gameState.code.length > 6){
-                gameState.code = "";
-            }
-        });
-        eight.on('pointerdown', () => {
-            gameState.code = gameState.code + "8";
-            codetext.destroy();
-            codetext = this.add.text( 100, 900, `Code : ${gameState.code}`, {fill: '#000000', fontSize: '100px'});
-            if(gameState.code.length > 6){
-                gameState.code = "";
-            }
-        });
-        nine.on('pointerdown', () => {
-            gameState.code = gameState.code + "9";
-            codetext.destroy();
-            codetext = this.add.text( 100, 900, `Code : ${gameState.code}`, {fill: '#000000', fontSize: '100px'});
-            if(gameState.code.length > 6){
-                gameState.code = "";
-            }
-        });
         gameState.createGame = function(scene){
             gameState.fatpoints = 0;
             text = scene.add.text( 100, 40, `Fat points : ${gameState.fatpoints}`, {fill: '#000000', fontSize: '40px'});
             gameState.iamfat = scene.sound.add('iamfat');
             gameState.joe = scene.add.image((window.innerWidth/2),(window.innerHeight/2), 'joepic').setScale(1).setInteractive();
+            gameState.chochcake = scene.add.image(window.innerWidth-100,(window.innerHeight/2), 'cake').setScale(0.3).setInteractive();
             gameState.joe.on('pointerdown', () => {
                 gameState.fatpoints += 1;
                 text.destroy();
-                    text = scene.add.text( 100, 40, `Fat points : ${gameState.fatpoints}`, {fill: '#000000', fontSize: '40px'});
+                text = scene.add.text( 100, 40, `Fat points : ${gameState.fatpoints}`, {fill: '#000000', fontSize: '40px'});
                 if(!gameState.iamfat.isPlaying){
                     gameState.iamfat.play();  
                 }
@@ -138,22 +57,34 @@ class MainScreen extends Phaser.Scene {
                     });
                 }
             });
+            gameState.caketalk =  0;
+            gameState.chochcake.on('pointerdown', () => {
+                if(caketext){
+                    caketext.destroy();
+                }
+                gameState.caketalk += 1;
+                if(gameState.caketalk == 1){
+                    caketext = scene.add.text( gameState.chochcake.x - 250, gameState.chochcake.y -100, `I think that fatso wants \nto eat me...`, {fill: '#000000', fontSize: '20px'});
+                }
+                else if(gameState.caketalk == 2){
+                    caketext = scene.add.text( gameState.chochcake.x - 250, gameState.chochcake.y -100, `Please don't feed me\n to him!`, {fill: '#000000', fontSize: '20px'});
+                }
+                else if(gameState.caketalk == 3){
+                    caketext = scene.add.text( gameState.chochcake.x - 250, gameState.chochcake.y -100, `NO... NOOOOO!`, {fill: '#000000', fontSize: '20px'});
+                }
+                else if(gameState.caketalk == 4){
+                    gameState.chochcake.destroy();
+                    caketext = scene.add.text( gameState.joe.x + 110, gameState.joe.y -200, `THAT CAKE WAS DELICIOUS!`, {fill: '#000000', fontSize: '20px'});
+                    gameState.fatpoints += 10000;
+                    text.destroy();
+                    text = scene.add.text( 100, 40, `Fat points : ${gameState.fatpoints}`, {fill: '#000000', fontSize: '40px'});
+                }
+            });
             gameState.gamecreated = true;
         }
+        gameState.createGame(this);
 	}
     update(){
-        if(gameState.gamecreated == false && gameState.code == 492758){
-            one.destroy();
-            two.destroy();
-            three.destroy();
-            four.destroy();
-            five.destroy();
-            six.destroy();
-            seven.destroy();
-            eight.destroy();
-            nine.destroy();
-            codetext.destroy();
-            gameState.createGame(this);
-        }
+        
     }
 }
